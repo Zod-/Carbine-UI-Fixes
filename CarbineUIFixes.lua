0ae3417845b32258765bac5535e4a170b65fa7ca
Julian Hangstörfer <jhangstoerfer@gmail.com>
2016-07-30 19:01:37 +0200

    v1.5.4.13938.0.2.0

--- CarbineUIFixes.lua
+++ CarbineUIFixes.lua
@@ -2,7 +2,7 @@ require "Apollo"
 
 local CarbineUIFixes = {
   uiMapperLib = "uiMapper:0.9.2",
-  version = "1.5.4.13938.0.1.0",
+  version = "1.5.4.13938.0.2.0",
   author = "Zod Bain@Jabbit",
   allFixes = {
     "ContextMenuPlayerFix"
