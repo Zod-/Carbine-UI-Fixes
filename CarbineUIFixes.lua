bad8a22e772334d0e4e92b5317ab8a4c6b99beb6
Julian Hangstörfer <jhangstoerfer@gmail.com>
2016-07-30 19:54:47 +0200

    v1.5.4.13938.0.3.0

--- CarbineUIFixes.lua
+++ CarbineUIFixes.lua
@@ -2,7 +2,7 @@ require "Apollo"
 
 local CarbineUIFixes = {
   uiMapperLib = "uiMapper:0.9.2",
-  version = "1.5.4.13938.0.2.1",
+  version = "1.5.4.13938.0.3.0",
   author = "Zod Bain@Jabbit",
   allFixes = {
     "WhisperFix",
