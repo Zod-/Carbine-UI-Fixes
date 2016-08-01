7382e5daa8672f28ec387bfcd265bfcae8119d58
Julian Hangstörfer <jhangstoerfer@gmail.com>
2016-08-02 01:00:48 +0200

    Load fixes as their own addons Don't have to pass them every single event. Removed OnSave and OnRestore events. Removed GetDependencies function. Closes #3

--- CarbineUIFixes.lua
+++ CarbineUIFixes.lua
@@ -44,31 +44,22 @@ end
 
 function CarbineUIFixes:Init()
   self:InitFixes()
-  self:GetDependencies()
-
-  Apollo.RegisterAddon(self, true, "Carbine UI Fixes", self.dependencies)
+  Apollo.RegisterAddon(self, true, "Carbine UI Fixes", {self.uiMapperLib})
+  self:RegisterFixesAsAddons()
 end
 
-function CarbineUIFixes:GetDependencies()
-  self.dependencies = {self.uiMapperLib}
-  local duplicate = {}
-  for k,fix in pairs(self.fixes) do
-    for _,v in ipairs(fix.dependencies) do
-      if not duplicate[v] then
-        table.insert(self.dependencies, v)
-        duplicate[v] = true
-      end
-    end
+function CarbineUIFixes:RegisterFixesAsAddons()
+  for key, fix in pairs(self.fixes) do
+    Apollo.RegisterAddon(
+      fix,
+      fix.configEnabled or false,
+      fix.configTitle or "",
+      fix.dependencies or {}
+    )
   end
 end
 
-function CarbineUIFixes:OnLoadFixes()
-  self:ExecOnFixes("OnLoad", self)
-end
-
 function CarbineUIFixes:OnLoad()
-  self:OnLoadFixes()
-
   local uiMapper = Apollo.GetPackage(self.uiMapperLib).tPackage
   Apollo.RegisterSlashCommand("cf", "OnConfigure", self)
   Apollo.RegisterSlashCommand("carbinefixes", "OnConfigure", self)
@@ -88,18 +79,6 @@ end
 function CarbineUIFixes:OnConfigure()
   if self.ui then
     self.ui.wndMain:Show(true,true)
-  end
-end
-
-function CarbineUIFixes:OnSave(saveLevel)
-  return self:ExecOnFixes("OnSave", saveLevel)
-end
-
-function CarbineUIFixes:OnRestore(saveLevel, data)
-  for key, val in pairs(data) do
-    if self.fixes[key] then
-      self:ExecOnFix(self.fixes[key], "OnRestore", saveLevel, data)
-    end
   end
 end
 
