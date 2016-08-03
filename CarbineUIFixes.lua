e116d303562b9c970fecc154226e4ee32ecfabec
Julian Hangstörfer <jhangstoerfer@gmail.com>
2016-08-03 04:03:39 +0200

    v1.5.4.13938.0.5.1

--- CarbineUIFixes.lua
+++ CarbineUIFixes.lua
@@ -4,7 +4,7 @@ require "GameLib"
 local GAME_VERSION = GameLib.GetVersionInfo()
 local CarbineUIFixes = {
   uiMapperLib = "uiMapper:0.9.2",
-  version = "1.5.4.13938.0.5.0",
+  version = "1.5.4.13938.0.5.1",
   author = "Zod Bain@Jabbit",
   allFixes = {
     "WhisperFix",
